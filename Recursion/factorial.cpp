e58e8aa18ce257d5c162bd974cf46ef086f99afd
Pradipmore12 <en19320803@git-india.edu.in>
2022-07-29 22:41:51 +0530

    formatDoc

--- Recursion/factorial.cpp
+++ Recursion/factorial.cpp
@@ -1,17 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int fact(int n){
-    if(n==2){
+int fact(int n)
+{
+    if (n == 2)
+    {
         return 2;
     }
-    int prevFact=fact(n-1);
+    int prevFact = fact(n - 1);
     return n * prevFact;
 }
-int main(){
+int main()
+{
 
     int n;
-    cin>>n;
-    cout<<fact(n)<<endl;
+    cin >> n;
+    cout << fact(n) << endl;
 
     return 0;
 }
