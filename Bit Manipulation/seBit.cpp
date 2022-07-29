8e94582ae5b6b445fbf4f1e4347c8f6f3d307c9b
Pradipmore12 <en19320803@git-india.edu.in>
2022-07-29 21:12:43 +0530

    formatDoc

--- Bit Manipulation/seBit.cpp
+++ Bit Manipulation/seBit.cpp
@@ -1,12 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int setBit(int n,int pos){
-    return (n|(1<<pos));
+int setBit(int n, int pos)
+{
+    return (n | (1 << pos));
 }
-int main(){
-    int n , pos;
-    cin>>n>>pos;
-    cout<<setBit(n,pos)<<endl;
+int main()
+{
+    int n, pos;
+    cin >> n >> pos;
+    cout << setBit(n, pos) << endl;
 
     return 0;
 }
