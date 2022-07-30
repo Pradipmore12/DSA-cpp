3b69c010a53d3364643adf60595745276151ecfb
Pradipmore12 <en19320803@git-india.edu.in>
2022-07-30 21:20:59 +0530

    freqChar

--- String/freqCharacter.cpp
+++ String/freqCharacter.cpp
@@ -1,23 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    string a="aqfeasscjissd";
+int main()
+{
+    string a = "aqfeasscjissd";
     int freq[26];
-    for(int i=0;i<26;i++){
-        freq[i]=0;
+    for (int i = 0; i < 26; i++)
+    {
+        freq[i] = 0;
     }
-    for(int i=0;i<a.size();i++){
-        freq[a[i]-'a']++;
+    for (int i = 0; i < a.size(); i++)
+    {
+        freq[a[i] - 'a']++;
     }
-    int max=0;
-    char ans='a';
-    for(int  i=0;i<26;i++){
-        if(freq[i]>max){
-            max=freq[i];
-            ans='a'+ i;
+    int max = 0;
+    char ans = 'a';
+    for (int i = 0; i < 26; i++)
+    {
+        if (freq[i] > max)
+        {
+            max = freq[i];
+            ans = 'a' + i;
         }
-
     }
-    cout<<max<<" "<<ans<<endl;
+    cout << max << " " << ans << endl;
     return 0;
 }
