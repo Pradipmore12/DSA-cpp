1d160352c61d9b00200ba7cdcf188489e871f404
Pradipmore12 <en19320803@git-india.edu.in>
2022-07-30 20:15:41 +0530

    lowerUpper

--- String/uToLtoU.cpp
+++ String/uToLtoU.cpp
@@ -1,17 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
 
     string sample;
-    cin>>sample;
-    for(int i=0;i<sample.length();i++){
-        if(sample[i]>='a' && sample[i]<='z'){
-            sample[i]=sample[i]-32;
+    cin >> sample;
+    for (int i = 0; i < sample.length(); i++)
+    {
+        if (sample[i] >= 'a' && sample[i] <= 'z')
+        {
+            sample[i] = sample[i] - 32;
         }
-        else if(sample[i]>='A' && sample[i]<='Z'){
-            sample[i]=sample[i]+32;
+        else if (sample[i] >= 'A' && sample[i] <= 'Z')
+        {
+            sample[i] = sample[i] + 32;
         }
     }
-    cout<<"Converted String :"<<sample<<endl;
+    cout << "Converted String :" << sample << endl;
     return 0;
 }
