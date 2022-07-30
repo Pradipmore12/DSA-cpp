7ee1ed5de8658e6954b8a771eb8c80bb5d8c61dd
Pradipmore12 <en19320803@git-india.edu.in>
2022-07-30 21:55:09 +0530

    incExe

---
+++ incExe.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int incExe(int n, int a, int b){
+    int divA=n/a;
+    int divB=n/b;
+    int divBoth=n/(a*b);
+    return divA + divB-divBoth;
+}
+int main(){
+    int n,a,b;
+    cin>>n>>a>>b;
+    cout<<incExe(n,a,b)<<endl;
+    return 0;
+}
