534eaedb3144425e401c3099d6e9c7be5df75304
Pradipmore12 <en19320803@git-india.edu.in>
2022-07-31 20:48:31 +0530

    powerRecursion

--- Recursion/power.cpp
+++ Recursion/power.cpp
@@ -1,15 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int power(int n, int p){
-    if(p==0){
+int power(int n, int p)
+{
+    if (p == 0)
+    {
         return 1;
     }
-    int prevPower = power(n,p-1);
+    int prevPower = power(n, p - 1);
     return n * prevPower;
 }
-int main(){
-    int n , p;
-    cin>>n>>p;
-    cout<<power(n,p)<<endl;
+int main()
+{
+    int n, p;
+    cin >> n >> p;
+    cout << power(n, p) << endl;
     return 0;
 }
