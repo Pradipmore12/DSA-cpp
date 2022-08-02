f2bb56745ceb62dcf9c7409d1719dc78d3c323fc
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-02 20:21:48 +0530

    uniqueInArray

--- Arrays/uniqueInArray.cpp
+++ Arrays/uniqueInArray.cpp
@@ -1,17 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     int size;
-    cin>>size;
+    cin >> size;
     int arr[100];
-    for(int i=0;i<size;i++){
-        cin>>arr[i];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
     }
-    int ans=0;
-    for(int i=0; i<size;i++){
+    int ans = 0;
+    for (int i = 0; i < size; i++)
+    {
 
-        ans=ans^arr[i];
+        ans = ans ^ arr[i];
     }
-    cout<<ans<<endl;
+    cout << ans << endl;
     return 0;
 }
