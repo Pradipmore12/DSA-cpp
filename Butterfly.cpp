b1534bc2a0c08d84210623b86fbef4dc93149f12
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-03 21:12:18 +0530

    butterfly

--- Butterfly.cpp
+++ Butterfly.cpp
@@ -1,46 +1,47 @@
 using namespace std;
-#include<iostream>
-int main(){
+#include <iostream>
+int main()
+{
     int n;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            cout<<"* ";
-
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
         }
-        for(int k=1;k<=n-i;k++){
-            cout<<"  ";
-
+        for (int k = 1; k <= n - i; k++)
+        {
+            cout << "  ";
         }
-        for(int l=1;l<=n-i;l++){
-            cout<<"  ";
-
+        for (int l = 1; l <= n - i; l++)
+        {
+            cout << "  ";
         }
-        for(int m=1;m<=i;m++){
-            cout<<"* ";
-
+        for (int m = 1; m <= i; m++)
+        {
+            cout << "* ";
         }
-        cout<<endl;
-        
+        cout << endl;
     }
-    for(int i=n;i>=1;i--){
-        for(int j=1;j<=i;j++){
-            cout<<"* ";
-
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
         }
-        for(int k=1;k<=n-i;k++){
-            cout<<"  ";
-
+        for (int k = 1; k <= n - i; k++)
+        {
+            cout << "  ";
         }
-        for(int l=1;l<=n-i;l++){
-            cout<<"  ";
-
+        for (int l = 1; l <= n - i; l++)
+        {
+            cout << "  ";
         }
-        for(int m=1;m<=i;m++){
-            cout<<"* ";
-
+        for (int m = 1; m <= i; m++)
+        {
+            cout << "* ";
         }
-        cout<<endl;
-        
+        cout << endl;
     }
 }
