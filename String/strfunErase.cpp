148976f19d517f1400196ff3427277168d4721ef
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-03 21:10:02 +0530

    strFunErase

--- String/strfunErase.cpp
+++ String/strfunErase.cpp
@@ -1,11 +1,12 @@
 using namespace std;
-#include<bits/stdc++.h>
-#include<iostream>
-#include<string>
-int main(){
-    string s1="pradip";
-    s1.erase(2,3);
-    cout<<s1<<endl;
-    
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+int main()
+{
+    string s1 = "pradip";
+    s1.erase(2, 3);
+    cout << s1 << endl;
+
     return 0;
 }
