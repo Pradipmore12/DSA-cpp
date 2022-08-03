d6e4aba2f8f2fbbf78dbfa235d7fbab1112849d9
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-03 21:09:02 +0530

    binarySearch

--- Arrays/binarySearch.cpp
+++ Arrays/binarySearch.cpp
@@ -1,30 +1,36 @@
 using namespace std;
-#include<iostream>
-int binarySearch(int arr[],int n, int key){
-    int s=0;
-    int e=n;
-    while(s<=e){
-        int mid=(s+e)/2;
-        if(arr[mid]==key){
+#include <iostream>
+int binarySearch(int arr[], int n, int key)
+{
+    int s = 0;
+    int e = n;
+    while (s <= e)
+    {
+        int mid = (s + e) / 2;
+        if (arr[mid] == key)
+        {
             return mid;
         }
-        else if(arr[mid]>key){
-            e=mid-1;
+        else if (arr[mid] > key)
+        {
+            e = mid - 1;
         }
         else
-            s=mid+1;
+            s = mid + 1;
     }
     return -1;
 }
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
     int key;
-    cin>>key;
-    cout<<binarySearch(arr,n,key)<<endl;
+    cin >> key;
+    cout << binarySearch(arr, n, key) << endl;
     return 0;
 }
