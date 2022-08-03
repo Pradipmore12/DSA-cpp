692c56ebf294f5046aed8119eb913526ab586469
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-03 21:11:17 +0530

    binaryDecimal

--- BinaryTodecimal.cpp
+++ BinaryTodecimal.cpp
@@ -1,22 +1,24 @@
 using namespace std;
-#include<math.h>
-#include<iostream>
-int binaryToDecimal(int n){
-    int ans=0;
-    int i=0;
-    while(n!=0){
-       int lastdigit=n%10;
-       ans+=lastdigit*pow(2,i);
-       n/=10;
-       i++;
-
+#include <math.h>
+#include <iostream>
+int binaryToDecimal(int n)
+{
+    int ans = 0;
+    int i = 0;
+    while (n != 0)
+    {
+        int lastdigit = n % 10;
+        ans += lastdigit * pow(2, i);
+        n /= 10;
+        i++;
     }
     return ans;
 }
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    cout<<binaryToDecimal(n);
+    cin >> n;
+    cout << binaryToDecimal(n);
 
     return 0;
 }
