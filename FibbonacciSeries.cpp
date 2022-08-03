3a97b149e9671dce9df4c41d0b47fe024d3cc126
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-03 21:41:30 +0530

    fibonacci

--- FibbonacciSeries.cpp
+++ FibbonacciSeries.cpp
@@ -1,17 +1,19 @@
 using namespace std;
-#include<iostream>
-int main(){
+#include <iostream>
+int main()
+{
     int n;
-    cin>>n;
-    int s=0,e=1;
-    int i=3;
-    cout<<s<<e;
-    while(i<=n){
-        
-        int next=s+e;
-        cout<<next;
-        s=e;
-        e=next;
+    cin >> n;
+    int s = 0, e = 1;
+    int i = 3;
+    cout << s << e;
+    while (i <= n)
+    {
+
+        int next = s + e;
+        cout << next;
+        s = e;
+        e = next;
         i++;
     }
     return 0;
