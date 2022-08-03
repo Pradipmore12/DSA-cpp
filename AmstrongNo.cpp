1c0e783dad4cf2d6f18da7e786b46a6e1e5c0134
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-03 21:10:37 +0530

    amstrongNo

--- AmstrongNo.cpp
+++ AmstrongNo.cpp
@@ -1,20 +1,22 @@
 using namespace std;
-#include<cmath>
-#include<iostream>
-int main(){
+#include <cmath>
+#include <iostream>
+int main()
+{
     int n;
-    cin>>n;
-    int originalno=n;
-    int sum=0;
-    while(n!=0){
-        int digit=n%10;
-        sum+=digit*digit*digit;
-        //sum+=pow(digit,3);
-        n/=10;
+    cin >> n;
+    int originalno = n;
+    int sum = 0;
+    while (n != 0)
+    {
+        int digit = n % 10;
+        sum += digit * digit * digit;
+        // sum+=pow(digit,3);
+        n /= 10;
     }
-    if(sum==originalno)
-        cout<<"Amstrong No"<<endl;
-    else    
-        cout<<"Not Amstrong No"<<endl;
+    if (sum == originalno)
+        cout << "Amstrong No" << endl;
+    else
+        cout << "Not Amstrong No" << endl;
     return 0;
 }
