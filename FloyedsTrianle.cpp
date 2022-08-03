f2aa7eba67a6f509c9c5591c3de2f56be3d1338f
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-03 21:42:00 +0530

    floyedTraongle

--- FloyedsTrianle.cpp
+++ FloyedsTrianle.cpp
@@ -1,14 +1,17 @@
 using namespace std;
-#include<iostream>
-int main(){
-    int n,num=1;
-    cin>>n;
-    for(int i=0;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            cout<<num<<" ";
+#include <iostream>
+int main()
+{
+    int n, num = 1;
+    cin >> n;
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << num << " ";
             num++;
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
