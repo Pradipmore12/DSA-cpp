42032cac5561022143546c81911b347c813b1c1d
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-03 21:40:28 +0530

    subStr

--- String/subStr.cpp
+++ String/subStr.cpp
@@ -1,8 +1,9 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    string pm="pradip";
-    cout<<pm.substr(1,3)<<endl;
+int main()
+{
+    string pm = "pradip";
+    cout << pm.substr(1, 3) << endl;
 
     return 0;
 }
