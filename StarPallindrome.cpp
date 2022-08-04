19c6f51c0c230ec62c98e69a2c073705abbc57cf
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-04 20:36:04 +0530

    starPallindrome

--- StarPallindrome.cpp
+++ StarPallindrome.cpp
@@ -1,31 +1,40 @@
 using namespace std;
 #include <iostream>
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    for(int i=0;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
-            cout<<"  ";
+    cin >> n;
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
         }
-        for(int j=n-i+1;j<=n;j++){
-            cout<<"* ";
+        for (int j = n - i + 1; j <= n; j++)
+        {
+            cout << "* ";
         }
-        for(int j=n+1;j<=n+i-1;j++){
-            cout<<"* ";
+        for (int j = n + 1; j <= n + i - 1; j++)
+        {
+            cout << "* ";
         }
-        cout<<endl;
+        cout << endl;
     }
-    for(int i=n;i>=1;i--){
-        for(int j=1;j<=n-i;j++){
-            cout<<"  ";
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
         }
-        for(int j=n-i+1;j<=n;j++){
-            cout<<"* ";
+        for (int j = n - i + 1; j <= n; j++)
+        {
+            cout << "* ";
         }
-        for(int j=n+1;j<=n+i-1;j++){
-            cout<<"* ";
+        for (int j = n + 1; j <= n + i - 1; j++)
+        {
+            cout << "* ";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
