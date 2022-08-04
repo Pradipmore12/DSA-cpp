0f73466bdcb9f051a696f6ebe58de157885f85ea
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-04 20:30:15 +0530

    PallindromeOrNot

--- PallindromeNOcheck.cpp
+++ PallindromeNOcheck.cpp
@@ -1,20 +1,22 @@
 using namespace std;
-#include<iostream>
-int main(){
+#include <iostream>
+int main()
+{
     int n;
-    cin>>n;
-    int originalno=n;
-    int reverse=0;
-    while(n!=0){
-        int lastdigit=n%10;
-        reverse=reverse*10+lastdigit;
-        n/=10;
-
+    cin >> n;
+    int originalno = n;
+    int reverse = 0;
+    while (n != 0)
+    {
+        int lastdigit = n % 10;
+        reverse = reverse * 10 + lastdigit;
+        n /= 10;
     }
-    if(reverse==originalno){
-        cout<<"pallindrome No"<<endl;
+    if (reverse == originalno)
+    {
+        cout << "pallindrome No" << endl;
     }
     else
-        cout<<"Not Pallindromw"<<endl;
+        cout << "Not Pallindromw" << endl;
     return 0;
 }
