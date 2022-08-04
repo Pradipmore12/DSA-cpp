fec1288c5ac0f3d54fbef027463a5b20bda18c57
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-04 20:33:15 +0530

    primeSecond

--- Prime2nd.cpp
+++ Prime2nd.cpp
@@ -1,19 +1,21 @@
 using namespace std;
-#include<iostream>
-int main(){
+#include <iostream>
+int main()
+{
     int n;
-    cin>>n;
-    bool prime=true;
-    for(int i=2;i<n;i++){
-        if(n%i==0){
-            cout<<"Not Prime"<<endl;
-            prime=false;
+    cin >> n;
+    bool prime = true;
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << "Not Prime" << endl;
+            prime = false;
             break;
-            
         }
     }
-    if(prime)
-    cout<<"Prime"<<endl;
+    if (prime)
+        cout << "Prime" << endl;
 
     return 0;
 }
