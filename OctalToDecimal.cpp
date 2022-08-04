a337ad23dab769298738ec919c20957c6119077f
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-04 20:29:28 +0530

    OctalTodecimal

--- OctalToDecimal.cpp
+++ OctalToDecimal.cpp
@@ -1,21 +1,24 @@
 using namespace std;
-#include<iostream>
-#include<bits/stdc++.h>
-#include<math.h>
-int octalToDecimal(int n){
-    int ans=0;
-    int i=0;
-    while(n!=0){
-        int digit=n%10;
-        ans+=digit*pow(8,i);
-        n/=10;
+#include <iostream>
+#include <bits/stdc++.h>
+#include <math.h>
+int octalToDecimal(int n)
+{
+    int ans = 0;
+    int i = 0;
+    while (n != 0)
+    {
+        int digit = n % 10;
+        ans += digit * pow(8, i);
+        n /= 10;
         i++;
     }
     return ans;
 }
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    cout<<octalToDecimal(n)<<endl;
+    cin >> n;
+    cout << octalToDecimal(n) << endl;
     return 0;
 }
