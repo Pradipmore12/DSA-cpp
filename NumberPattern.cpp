90f55891902ec9163d3642a4ff1f02e44d029620
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-04 20:28:25 +0530

    numberPattern

--- NumberPattern.cpp
+++ NumberPattern.cpp
@@ -1,17 +1,21 @@
 using namespace std;
-#include<iostream>
-int main(){
+#include <iostream>
+int main()
+{
     int n;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
-            cout<<" ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
         }
-        for(int k=1;k<=i;k++){
-            cout<<k;
+        for (int k = 1; k <= i; k++)
+        {
+            cout << k;
         }
-        cout<<endl;
+        cout << endl;
     }
-    
+
     return 0;
 }
