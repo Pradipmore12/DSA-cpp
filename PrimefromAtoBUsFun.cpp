9d9aa12960c34f641ec07000bdea53eba3fd44f1
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-04 20:33:50 +0530

    primeFromAtoB

--- PrimefromAtoBUsFun.cpp
+++ PrimefromAtoBUsFun.cpp
@@ -1,23 +1,26 @@
 using namespace std;
-#include<iostream>
-bool isPrime(int num){
-    for(int i=2;i<num;i++){
-        if(num==1 || num%i==0){
+#include <iostream>
+bool isPrime(int num)
+{
+    for (int i = 2; i < num; i++)
+    {
+        if (num == 1 || num % i == 0)
+        {
             return false;
         }
     }
     return true;
 }
-int main(){
-    int a,b;
-    cin>>a>>b;
-    for(int i=a;i<=b;i++){
-        if(isPrime(i)){
-             cout<<i<<endl;
-
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    for (int i = a; i <= b; i++)
+    {
+        if (isPrime(i))
+        {
+            cout << i << endl;
         }
-           
-
     }
     return 0;
 }
