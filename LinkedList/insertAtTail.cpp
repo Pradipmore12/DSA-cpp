2544407f43565d813989e59df879000037b677bf
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-05 21:46:20 +0530

    InsertAtTail

--- LinkedList/insertAtTail.cpp
+++ LinkedList/insertAtTail.cpp
@@ -1,45 +1,51 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Node{
-    public:
+class Node
+{
+public:
     int data;
-    Node * next;
+    Node *next;
 
-    Node(int data){
-        this ->data=data;
-        this->next=NULL;
-
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
     }
 };
 
-void print(Node* &head){
-    Node* temp=head;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
-        temp=temp->next;
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
     }
-    cout<<endl;
+    cout << endl;
 }
 
-void insertAtHead(Node* &head,int data){
-    Node *temp=new Node(data);
-    temp->next=head;
-    head=temp;
+void insertAtHead(Node *&head, int data)
+{
+    Node *temp = new Node(data);
+    temp->next = head;
+    head = temp;
 }
-void insertAtTail(Node* &tail,int data){
-    Node *temp=new Node(data);
-    tail->next=temp;
-    tail=temp;
+void insertAtTail(Node *&tail, int data)
+{
+    Node *temp = new Node(data);
+    tail->next = temp;
+    tail = temp;
 }
-int main(){
+int main()
+{
 
-    Node* node1=new Node(20);
-    Node* head=node1;
-    Node* tail=node1;
+    Node *node1 = new Node(20);
+    Node *head = node1;
+    Node *tail = node1;
 
-    insertAtHead(head,10);
+    insertAtHead(head, 10);
     print(head);
-    insertAtTail(tail,30);
+    insertAtTail(tail, 30);
     print(head);
 
     return 0;
