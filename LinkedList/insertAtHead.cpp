d00f5f7f5bc672e91a99828d153bf4c40b14d791
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-05 21:01:50 +0530

    insertAtHead

--- LinkedList/insertAtHead.cpp
+++ LinkedList/insertAtHead.cpp
@@ -13,40 +13,40 @@ public:
         this->data = data;
         this->next = NULL;
     }
-
-    
 };
-void insertAtHead(Node* &head,int data){
-    Node *temp=new Node(data);
-    temp ->next=head;
-    head=temp;
-
+void insertAtHead(Node *&head, int data)
+{
+    Node *temp = new Node(data);
+    temp->next = head;
+    head = temp;
 }
-void print(Node* &head){
-        Node *temp =head;
-        while(head!=NULL){
-            cout<<temp ->data;
-            temp = temp ->next;
-        }
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (head != NULL)
+    {
+        cout << temp->data;
+        temp = temp->next;
+    }
 }
 int main()
 {
-    //create a new node
+    // create a new node
     Node *node1 = new Node(20);
-    //cout << node1->data << " " << node1->next << endl;
+    // cout << node1->data << " " << node1->next << endl;
 
-    //head pointed to node1
+    // head pointed to node1
 
     Node *head = node1;
-    
-    //print elements
+
+    // print elements
     print(head);
 
-    //insert at head
+    // insert at head
 
-    insertAtHead(head,10);
+    insertAtHead(head, 10);
 
-    //print after insertion
+    // print after insertion
 
     print(head);
 
