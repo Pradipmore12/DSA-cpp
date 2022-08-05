1cf45d0732ff08874554a5dea59223b1d15b8994
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-05 21:45:06 +0530

    insertAtTail

---
+++ LinkedList/insertAtTail.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node * next;
+
+    Node(int data){
+        this ->data=data;
+        this->next=NULL;
+
+    }
+};
+
+void print(Node* &head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+void insertAtHead(Node* &head,int data){
+    Node *temp=new Node(data);
+    temp->next=head;
+    head=temp;
+}
+void insertAtTail(Node* &tail,int data){
+    Node *temp=new Node(data);
+    tail->next=temp;
+    tail=temp;
+}
+int main(){
+
+    Node* node1=new Node(20);
+    Node* head=node1;
+    Node* tail=node1;
+
+    insertAtHead(head,10);
+    print(head);
+    insertAtTail(tail,30);
+    print(head);
+
+    return 0;
+}
