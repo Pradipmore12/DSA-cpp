f417f8702c22c0106a86b710c8c47052af8ac401
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-05 21:01:26 +0530

    insertAtHead

---
+++ LinkedList/insertAtHead.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// initiate a Node class
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    
+};
+void insertAtHead(Node* &head,int data){
+    Node *temp=new Node(data);
+    temp ->next=head;
+    head=temp;
+
+}
+void print(Node* &head){
+        Node *temp =head;
+        while(head!=NULL){
+            cout<<temp ->data;
+            temp = temp ->next;
+        }
+}
+int main()
+{
+    //create a new node
+    Node *node1 = new Node(20);
+    //cout << node1->data << " " << node1->next << endl;
+
+    //head pointed to node1
+
+    Node *head = node1;
+    
+    //print elements
+    print(head);
+
+    //insert at head
+
+    insertAtHead(head,10);
+
+    //print after insertion
+
+    print(head);
+
+    return 0;
+}
