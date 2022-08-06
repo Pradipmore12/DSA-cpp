59e68480f0535762d3524f8cd651d1d74aace3ae
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-06 19:22:19 +0530

    insertAtMiddle

--- LinkedList/insertAtMiddle.cpp
+++ LinkedList/insertAtMiddle.cpp
@@ -1,49 +1,54 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Node{
-    public:
+class Node
+{
+public:
     int data;
-    Node* next;
+    Node *next;
 
-    Node(int data){
-        this->data=data;
-        this->next=NULL;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
     }
 };
-void print(Node* &head){
-    Node* temp=head;
-    while (temp!=0)
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != 0)
     {
-        cout<<temp->data<<" ";
-        temp=temp->next;
+        cout << temp->data << " ";
+        temp = temp->next;
     }
-    cout<<endl;
-    
+    cout << endl;
 }
-void insertAtMiddle(Node* &head,int pos,int data){
-    Node* temp=head;
-    int count=1;
-    while(count<pos){
-        temp=temp->next;
+void insertAtMiddle(Node *&head, int pos, int data)
+{
+    Node *temp = head;
+    int count = 1;
+    while (count < pos - 1)
+    {
+        temp = temp->next;
         count++;
     }
-    //Insertion node creation
+    // Insertion node creation
 
-    Node* nodeToInsert=new Node(data);
-    nodeToInsert->next=temp->next;
-    temp->next=nodeToInsert;
+    Node *nodeToInsert = new Node(data);
+    nodeToInsert->next = temp->next;
+    temp->next = nodeToInsert;
 }
 
-int main(){
-    Node* node1=new Node(10);
-    Node* node2=new Node(20);
-    Node * head=node1;
-    node1->next=node2;
+int main()
+{
+    Node *node1 = new Node(10);
+    Node *node2 = new Node(20);
+    Node *node3 = new Node(80);
+    Node *head = node1;
+    node1->next = node2;
+    node2->next = node3;
     print(head);
-    insertAtMiddle(head,2,30);
+    insertAtMiddle(head, 3, 30);
     print(head);
-
 
     return 0;
-
 }
