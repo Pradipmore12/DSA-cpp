ff1cb6977bc67787d45324b818333d743f498062
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-07 21:15:00 +0530

    DoublyLLStart

--- LinkedList/doublyLL/implement.cpp
+++ LinkedList/doublyLL/implement.cpp
@@ -1,41 +1,46 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Node{
-    public:
+class Node
+{
+public:
     int data;
     Node *prev;
     Node *next;
 
-    Node(int data){
-        this->data=data;
-        this->next=NULL;
-        this->prev=NULL;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+        this->prev = NULL;
     }
 };
-void print(Node *&head){
+void print(Node *&head)
+{
     Node *temp = head;
-    while(temp!=NULL){
-        cout<<temp->data;
-        temp=temp->next;
+    while (temp != NULL)
+    {
+        cout << temp->data;
+        temp = temp->next;
     }
-    cout<<endl;
+    cout << endl;
 }
-int length(Node *&head){
-    int len=1;
-    Node *temp=head;
-    while(temp!=NULL){
-        temp=temp->next;
+int length(Node *&head)
+{
+    int len = 1;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        temp = temp->next;
         len++;
-
     }
     return len;
 }
-int main(){
-    Node *node1=new Node(10);
+int main()
+{
+    Node *node1 = new Node(10);
     Node *head = node1;
     print(head);
-    cout<<length(head)<<endl;
-
+    cout << length(head) << endl;
 
     return 0;
 }
