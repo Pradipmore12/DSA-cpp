9137be02ebdf04fad4756310cdab6b6031fe0081
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-07 21:53:40 +0530

    DLLinsertAtTail

---
+++ LinkedList/doublyLL/insertAtTAil.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+void insertAtHead(Node *&head, int data)
+{
+    Node *temp = new Node(data);
+    //add your node before head Node
+    temp->next = head;
+    //change prev pointer of head to temp
+    head->prev = temp;
+    //update head by temp/newly created Node
+    head = temp;
+}
+
+//Insertion at TAil
+
+void insertAtTail(Node *&tail,int data){
+    Node *temp=new Node(data);
+
+    
+}
+int main()
+{
+    Node *node1 = new Node(10);
+    Node *head = node1;
+    Node *tail = node1;
+    // head->prev=NULL;
+    print(head);
+    insertAtHead(head, 20);
+    print(head);
+    insertAtTail(tail,30);
+    print(head);
+
+    return 0;
+}
