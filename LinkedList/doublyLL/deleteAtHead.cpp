ecd0138cdd6d7917640b3c06f8f26a6f763a2cd1
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-08 20:12:30 +0530

    deleteAtHead

---
+++ LinkedList/doublyLL/deleteAtHead.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+
+};
+void print(Node *&head){
+    Node *temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+void deleteAtHead(Node *&head){
+    head=head->next;
+    head->prev=NULL;
+}
+int main(){
+    Node *node1=new Node(10);
+    Node *node2=new Node(20);
+    Node *node3=new Node(30);
+    node1->next=node2;
+    node2->prev=node1;
+    node2->next=node3;
+    node3->prev=node2;
+    Node *head=node1;
+    print(head);
+    deleteAtHead(head);
+    print(head);
+
+
+    return 0;
+}
