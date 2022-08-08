03970585f5cf1350f4de8de1c4efb0f375b985b7
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-08 20:54:51 +0530

    deleteAtTail

--- LinkedList/doublyLL/deleteAtTail.cpp
+++ LinkedList/doublyLL/deleteAtTail.cpp
@@ -1,41 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Node{
-    public:
+class Node
+{
+public:
     int data;
     Node *next;
     Node *prev;
 
-    Node(int data){
-        this->data=data;
-        this->next=NULL;
-        this->prev=NULL;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+        this->prev = NULL;
     }
 };
-void print(Node *head){
-    Node *temp=head;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
-        temp=temp->next;
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
     }
-    cout<<endl;
+    cout << endl;
 }
-void deleteAtTail(Node *&tail){
-    tail=tail->prev;
-    tail->next=NULL;
+void deleteAtTail(Node *&tail)
+{
+    tail = tail->prev;
+    tail->next = NULL;
 }
-int main(){
+int main()
+{
 
-    Node *node1=new Node(10);
-    Node *head=node1;
-    Node *node2=new Node(20);
-    Node *node3=new Node(30);
-    Node *tail=node3;
+    Node *node1 = new Node(10);
+    Node *head = node1;
+    Node *node2 = new Node(20);
+    Node *node3 = new Node(30);
+    Node *tail = node3;
 
-    node1->next=node2;
-    node2->prev=node1;
-    node2->next=node3;
-    node3->prev=node2;
+    node1->next = node2;
+    node2->prev = node1;
+    node2->next = node3;
+    node3->prev = node2;
 
     print(head);
     deleteAtTail(tail);
