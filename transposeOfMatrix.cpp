2c319af9da8d0defb16783d0a34e3118ed1b488c
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-09 16:31:42 +0530

    TransposeMatrix

--- transposeOfMatrix.cpp
+++ transposeOfMatrix.cpp
@@ -1,33 +1,43 @@
 using namespace std;
-#include<bits/stdc++.h>
-int main(){
+#include <bits/stdc++.h>
+int main()
+{
     int m;
-    cin>>m;
+    cin >> m;
     int arr[m][m];
     int transpose[m][m];
-    for(int i=0;i<m;i++){
-        for(int j=0;j<m;j++){
-            cin>>arr[i][j];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
         }
     }
-    for(int i=0;i<m;i++){
-        for(int j=0;j<m;j++){
-            cout<<arr[i][j]<<" ";
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
-    cout<<endl<<endl;
-    for(int i=0;i<m;i++){
-        for(int j=0;j<m;j++){
+    cout << endl
+         << endl;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
 
-            transpose[j][i]=arr[i][j];
+            transpose[j][i] = arr[i][j];
         }
     }
-        for(int i=0;i<m;i++){
-        for(int j=0;j<m;j++){
-            cout<<transpose[i][j]<<" ";
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << transpose[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
