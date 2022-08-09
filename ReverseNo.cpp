19eb9ffdff67a9f25671548a19c63585164045d1
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-09 16:32:36 +0530

    ReverseNo

--- ReverseNo.cpp
+++ ReverseNo.cpp
@@ -1,14 +1,16 @@
 using namespace std;
-#include<iostream>
-int main(){
+#include <iostream>
+int main()
+{
     int n;
-    cin>>n;
-    int reverse=0;
-    while(n!=0){
-        int lastdigit=n%10;
-        reverse=reverse*10+lastdigit;
-        n/=10;
+    cin >> n;
+    int reverse = 0;
+    while (n != 0)
+    {
+        int lastdigit = n % 10;
+        reverse = reverse * 10 + lastdigit;
+        n /= 10;
     }
-    cout<<reverse<<endl;
+    cout << reverse << endl;
     return 0;
 }
