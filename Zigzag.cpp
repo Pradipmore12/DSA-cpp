0d357bc55cb68e3f981a283e58860abc40bb612c
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-09 16:30:09 +0530

    Zigzag

--- Zigzag.cpp
+++ Zigzag.cpp
@@ -1,16 +1,19 @@
 using namespace std;
-#include<iostream>
-int main(){
+#include <iostream>
+int main()
+{
     int n;
-    cin>>n;
-    for(int i=1;i<=3;i++){
-        for(int j=1;j<=n;j++){
-            if((i+j)%4==0 || (i==2 && j%4==0))
-            cout<<"*";
-            else    cout<<" ";
+    cin >> n;
+    for (int i = 1; i <= 3; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if ((i + j) % 4 == 0 || (i == 2 && j % 4 == 0))
+                cout << "*";
+            else
+                cout << " ";
         }
-        cout<<endl;
-
+        cout << endl;
     }
     return 0;
 }
