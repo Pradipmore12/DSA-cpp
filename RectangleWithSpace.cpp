79a1fc21de221b2444a5dc8fcca8e7bc41cff2dc
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-09 16:33:04 +0530

    RectangleSpace

--- RectangleWithSpace.cpp
+++ RectangleWithSpace.cpp
@@ -1,17 +1,21 @@
 using namespace std;
-#include<iostream>
-int main(){
-    int row,col;
-    cin>>row>>col;
-    for(int i=1;i<=row;i++){
-        for(int j=1;j<=col;j++){
-            if(i==1||i==row ||j==1 ||j==col){
-                cout<<"*";
+#include <iostream>
+int main()
+{
+    int row, col;
+    cin >> row >> col;
+    for (int i = 1; i <= row; i++)
+    {
+        for (int j = 1; j <= col; j++)
+        {
+            if (i == 1 || i == row || j == 1 || j == col)
+            {
+                cout << "*";
             }
             else
-            cout<<" ";
+                cout << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
