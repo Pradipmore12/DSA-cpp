30b86b28b0d646a7cd5ca8eb78481ea968cd2929
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-10 22:23:36 +0530

    FormatDoc

--- Arrays/2DArray/transposeMatrixusingDiagonal.cpp
+++ Arrays/2DArray/transposeMatrixusingDiagonal.cpp
@@ -1,39 +1,45 @@
 using namespace std;
-#include<bits/stdc++.h>
-int main(){
+#include <bits/stdc++.h>
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n][n];
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
 
-            cin>>arr[i][j];
-
+            cin >> arr[i][j];
         }
     }
-      for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
 
-            cout<<arr[i][j]<<" ";
-            
+            cout << arr[i][j] << " ";
         }
-        cout<<"\n";
+        cout << "\n";
     }
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            int temp=arr[i][j];
-            arr[i][j]=arr[j][i];
-            arr[j][i]=temp;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
         }
     }
 
-     for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
 
-            cout<<arr[i][j]<<" ";
-            
+            cout << arr[i][j] << " ";
         }
-        cout<<"\n";
+        cout << "\n";
     }
     return 0;
 }
