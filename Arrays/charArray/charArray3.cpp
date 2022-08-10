df3ebc80f02e77129f2d8a8dee582611a7ada710
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-10 22:25:46 +0530

    FormatDoc

--- Arrays/charArray/charArray3.cpp
+++ Arrays/charArray/charArray3.cpp
@@ -1,22 +1,26 @@
 using namespace std;
-#include<bits/stdc++.h>
-int main(){
+#include <bits/stdc++.h>
+int main()
+{
     int n;
-    cin>>n;
-    char arr[n+1];
-    cin>>arr;
-    bool check=true;
-    for(int i=0;i<n;i++){
-        if(arr[i]!=arr[n-1-i]){
-            check=false;
+    cin >> n;
+    char arr[n + 1];
+    cin >> arr;
+    bool check = true;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != arr[n - 1 - i])
+        {
+            check = false;
         }
     }
 
-    if(check){
-        cout<<"pallindrome"<<endl;
+    if (check)
+    {
+        cout << "pallindrome" << endl;
     }
     else
-        cout<<"not a pallindrome"<<endl;
+        cout << "not a pallindrome" << endl;
 
     return 0;
 }
