c6d8d273469ec02aea32f16adfbd31a8af24d67e
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-10 22:26:59 +0530

    CharArrayStart

--- Arrays/charArray/takingInputOutput.cpp
+++ Arrays/charArray/takingInputOutput.cpp
@@ -1,13 +1,14 @@
 using namespace std;
-#include<bits/stdc++.h>
-int main(){
-    char arr[100]="Pradip";
-    int i=0;
-    while(arr[i]!='\0'){
-        cout<<arr[i];
+#include <bits/stdc++.h>
+int main()
+{
+    char arr[100] = "Pradip";
+    int i = 0;
+    while (arr[i] != '\0')
+    {
+        cout << arr[i];
         i++;
     }
-
 
     return 0;
 }
