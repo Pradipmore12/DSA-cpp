a7925fac923f6da7d6e637e4bd6876994d3d25f0
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-11 20:42:44 +0530

    format

--- Arrays/Sort/arrayChallegeSumOfallSubaarays.cpp
+++ Arrays/Sort/arrayChallegeSumOfallSubaarays.cpp
@@ -1,18 +1,22 @@
 using namespace std;
-#include<bits/stdc++.h>
-int main(){
-    int n; 
-    cin>>n;
+#include <bits/stdc++.h>
+int main()
+{
+    int n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
     int sum;
-    for(int i=0;i<n;i++){
-        sum=0;
-        for(int j=i;j<n;j++){
-            sum+=arr[j];
-            cout<<sum<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        sum = 0;
+        for (int j = i; j < n; j++)
+        {
+            sum += arr[j];
+            cout << sum << endl;
         }
     }
     return 0;
