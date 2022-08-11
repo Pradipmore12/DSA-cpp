260a8f3deabb712f04625b9aa45eac68af2c5206
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-11 20:43:09 +0530

    format

--- Arrays/Sort/arraysChallengeLongestArithmeticSubaaray.cpp
+++ Arrays/Sort/arraysChallengeLongestArithmeticSubaaray.cpp
@@ -1,27 +1,32 @@
 using namespace std;
-#include<bits/stdc++.h>
-int main(){
+#include <bits/stdc++.h>
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    
-    int current=2;
-    int j=2;
-    int pd=arr[1]-arr[0];
-    while(j<n){
-        if(arr[j]-arr[j-1]==pd){
+
+    int current = 2;
+    int j = 2;
+    int pd = arr[1] - arr[0];
+    while (j < n)
+    {
+        if (arr[j] - arr[j - 1] == pd)
+        {
             current++;
         }
-        else {
-            pd=arr[j]-arr[j-1];
-            current=2;
+        else
+        {
+            pd = arr[j] - arr[j - 1];
+            current = 2;
         }
-        
+
         j++;
     }
-    cout<<current<<endl;
+    cout << current << endl;
     return 0;
 }
