f0a252c4f97765b67f128c47b1a608adea6b5dc1
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-11 20:45:33 +0530

    format

--- Arrays/Sort/selectionSortArray.cpp
+++ Arrays/Sort/selectionSortArray.cpp
@@ -1,28 +1,34 @@
 using namespace std;
-#include<iostream>
-void selectionSort(int arr[], int n){
-    for(int i=0;i<n-1;i++){
-        for(int j=i+1; j<n;j++){
-            if(arr[i]>arr[j]){
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
+#include <iostream>
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
-        
     }
-    for(int i=0;i<n;i++){
-            cout<<arr[i]<<" ";
-        }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
     return;
 }
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    selectionSort(arr,n);
+    selectionSort(arr, n);
     return 0;
 }
