71f9cc82ff5b282e0d206108ae36db7be4391054
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-12 21:54:25 +0530

    Format

--- HalfPyramidNumbers.cpp
+++ HalfPyramidNumbers.cpp
@@ -1,13 +1,16 @@
 using namespace std;
-#include<iostream>
-int main(){
+#include <iostream>
+int main()
+{
     int n;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            cout<<i;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i;
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
