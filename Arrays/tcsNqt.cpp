e1c30c7a02f37b9b3dd7b66b52888059ec14ddfb
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-12 21:51:45 +0530

    Format

--- Arrays/tcsNqt.cpp
+++ Arrays/tcsNqt.cpp
@@ -3,12 +3,15 @@ using namespace std;
 
 int main()
 {
-    int n,j=0; cin>>n;
-    int a[n]={0};
-    for(int i=0;i<n;i++)
+    int n, j = 0;
+    cin >> n;
+    int a[n] = {0};
+    for (int i = 0; i < n; i++)
     {
-        cin>>a[j];
-        if(a[j]!=0) j++;
+        cin >> a[j];
+        if (a[j] != 0)
+            j++;
     }
-    for(int i=0;i<n;i++) cout<<a[i]<<" ";
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
 }
