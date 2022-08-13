35e789151ed3fe8ad34ef8038b00bd334ff8bb3d
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-13 22:05:12 +0530

    Format

--- PrimeNo.cpp
+++ PrimeNo.cpp
@@ -1,16 +1,19 @@
 using namespace std;
-#include<iostream>
-int main(){
+#include <iostream>
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int i;
-    for(i=2;i<n;i++){
-        if(n%i==0){
-            cout<<"Not Prime";
+    for (i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << "Not Prime";
             break;
         }
     }
-    if(i==n)
-        cout<<"Prime";
+    if (i == n)
+        cout << "Prime";
     return 0;
 }
