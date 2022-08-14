9888cfec7065d9e93b1616383eba9fc5ddecbdce
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-14 21:04:59 +0530

    Format

--- invertedPattern.cpp
+++ invertedPattern.cpp
@@ -1,13 +1,16 @@
 using namespace std;
-#include<iostream>
-int main(){
+#include <iostream>
+int main()
+{
     int n;
-    cin>>n;
-    for(int i=n;i>=1;i--){
-        for(int j=1;j<=i;j++){
-            cout<<j;
+    cin >> n;
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
