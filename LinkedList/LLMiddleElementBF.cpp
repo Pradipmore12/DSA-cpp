53bedb63cb40c88a39f058d123552b63250b525c
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-18 16:02:20 +0530

    MiddleElOp

--- LinkedList/LLMiddleElementBF.cpp
+++ LinkedList/LLMiddleElementBF.cpp
@@ -1,74 +1,76 @@
-#include<bits/stdC++.h>
+#include <bits/stdC++.h>
 using namespace std;
-class Node{
-    public:
+class Node
+{
+public:
     int data;
     Node *next;
-    Node(int data){
-        this->data=data;
-        this->next=NULL;
-
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
     }
 };
-void print(Node *&head){
-    Node *temp=head;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
-        temp=temp->next;
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
     }
-    cout<<endl;
+    cout << endl;
 }
-void insertAtTail(Node *&head,Node *&tail,int data){
-    Node *temp=new Node(data);
-    if(tail==NULL){
-        tail=temp;
-        head=temp;
+void insertAtTail(Node *&head, Node *&tail, int data)
+{
+    Node *temp = new Node(data);
+    if (tail == NULL)
+    {
+        tail = temp;
+        head = temp;
     }
-    else{
-        tail->next=temp;
-        tail=temp;
-
+    else
+    {
+        tail->next = temp;
+        tail = temp;
     }
-
 }
-int printMiddle(Node *&head){
-    int count=0;
-    Node *temp=head;
-    while(temp!=NULL){
-        temp=temp->next;
+int printMiddle(Node *&head)
+{
+    int count = 0;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        temp = temp->next;
         count++;
     }
-    int ans =count/2;
-    Node *start=head;
-    count=0;
-    while(count<ans){
-        start=start->next;
+    int ans = count / 2;
+    Node *start = head;
+    count = 0;
+    while (count < ans)
+    {
+        start = start->next;
         count++;
     }
-    
+
     return start->data;
-    
 }
-int main(){
-    Node *head=NULL;
-    Node *tail=NULL;
-    int n,i=0;
-    cout<<"how many Elements"<<endl;
-    cin>>n;
-    while(i<n)
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int n, i = 0;
+    cout << "how many Elements" << endl;
+    cin >> n;
+    while (i < n)
     {
         int data;
-        cin>>data;
-        insertAtTail(head,tail,data);
+        cin >> data;
+        insertAtTail(head, tail, data);
         i++;
-    
     }
     print(head);
-    cout<<"Middle Element is :"<<printMiddle(head)<<endl;
-
-        
-    
-    
+    cout << "Middle Element is :" << printMiddle(head) << endl;
 
     return 0;
 }
---
+++ LinkedList/LLMiddleOptimized.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdC++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+
+    }
+};
+void print(Node *&head){
+    Node *temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+void insertAtTail(Node *&head,Node *&tail,int data){
+    Node *temp=new Node(data);
+    if(tail==NULL){
+        tail=temp;
+        head=temp;
+    }
+    else{
+        tail->next=temp;
+        tail=temp;
+
+    }
+
+}
+Node *printMiddle(Node *&head){
+    Node *pro=head->next;
+    Node *nube=head;
+    while(pro!=NULL){
+        pro=pro->next;
+        if(pro!=NULL){
+            pro=pro->next;
+        }
+        nube=nube->next;
+    }
+
+    
+    return nube;
+    
+}
+int main(){
+    Node *head=NULL;
+    Node *tail=NULL;
+    int n,i=0;
+    cout<<"how many Elements"<<endl;
+    cin>>n;
+    while(i<n)
+    {
+        int data;
+        cin>>data;
+        insertAtTail(head,tail,data);
+        i++;
+    
+    }
+    print(head);
+    cout<<"Middle Element is :"<<printMiddle(head)->data<<endl;
+
+        
+    
+    
+
+    return 0;
+}
