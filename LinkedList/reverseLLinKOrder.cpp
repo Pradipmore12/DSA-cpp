2dd084ec42d8c3378a461ffa580d0aa0c063cc58
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-18 20:44:33 +0530

    reverseLLinKOrder

--- LinkedList/reverseLLinKOrder.cpp
+++ LinkedList/reverseLLinKOrder.cpp
@@ -1,69 +1,81 @@
-#include<bits/stdC++.h>
+#include <bits/stdC++.h>
 using namespace std;
-class Node{
-    public:
+class Node
+{
+public:
     int data;
     Node *next;
-    Node(int data){
-        this->data=data;
-        this->next=NULL;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
     }
 };
-void print(Node *&head){
-    Node *temp=head;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
-        temp=temp->next;
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
     }
-    cout<<endl;
+    cout << endl;
 }
-void insertAtTail(Node *&head,Node *&tail,int data){
-    Node *temp=new Node(data);
-    if(head==NULL || tail ==NULL){
-        head=temp;
-        tail=temp;
+void insertAtTail(Node *&head, Node *&tail, int data)
+{
+    Node *temp = new Node(data);
+    if (head == NULL || tail == NULL)
+    {
+        head = temp;
+        tail = temp;
     }
-    else{
-        tail->next=temp;
-        tail=temp;
+    else
+    {
+        tail->next = temp;
+        tail = temp;
     }
 }
-Node *reverseOrderK(Node *&head,int k){
-    Node *prev=NULL;
-    Node *curr=head;
-    Node *next=NULL;
-    int count=0;
-    while(head!=NULL && count<k){
-        next=curr->next;
-        curr->next=prev;
-        prev=curr;
-        curr=next;
+Node *reverseOrderK(Node *&head, int k)
+{
+    Node *prev = NULL;
+    Node *curr = head;
+    Node *next = NULL;
+    int count = 0;
+    while (head != NULL && count < k)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
         count++;
     }
 
-    if(head!=NULL){
-        head->next=reverseOrderK(next,k);
+    if (head != NULL)
+    {
+        head->next = reverseOrderK(next, k);
     }
     return prev;
 }
-int main(){
+int main()
+{
 
-    Node *head=NULL;
-    Node *tail=NULL;
+    Node *head = NULL;
+    Node *tail = NULL;
     int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
         int data;
-        cin>>data;
-        insertAtTail(head,tail,data);
+        cin >> data;
+        insertAtTail(head, tail, data);
     }
     print(head);
-    cout<<endl<<"enter K"<<endl;
+    cout << endl
+         << "enter K" << endl;
     int k;
-    cin>>k;
-    Node *reverse=reverseOrderK(head,k);
+    cin >> k;
+    Node *reverse = reverseOrderK(head, k);
     print(reverse);
-    
 
     return 0;
 }
