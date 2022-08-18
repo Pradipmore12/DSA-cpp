1a9b13f05ea79552ea8a325cd17892b837f0335e
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-18 21:30:11 +0530

    reverseDoublyLL

--- LinkedList/doublyLL/reverseDoublyLL.cpp
+++ LinkedList/doublyLL/reverseDoublyLL.cpp
@@ -1,66 +1,70 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Node{
-    public:
+class Node
+{
+public:
     int data;
     Node *next;
     Node *prev;
 
-    Node(int data){
-        this->data=data;
-        this->next=NULL;
-        this->prev=NULL;
-
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+        this->prev = NULL;
     }
 };
-void print(Node *&head){
-    Node *temp=head;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
-        temp=temp->next;
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
     }
-    cout<<endl;
+    cout << endl;
 }
-void inserAtTail(Node *&head,Node *&tail,int data){
-    if(tail==NULL){
-        Node *temp=new Node(data);
-        tail=temp;
-        head=temp;
-
+void inserAtTail(Node *&head, Node *&tail, int data)
+{
+    if (tail == NULL)
+    {
+        Node *temp = new Node(data);
+        tail = temp;
+        head = temp;
     }
-    else{
-        Node *temp=new Node(data);
-        temp->prev=tail;
-        tail->next=temp;
-        tail=temp;
-
+    else
+    {
+        Node *temp = new Node(data);
+        temp->prev = tail;
+        tail->next = temp;
+        tail = temp;
     }
 }
-Node *reverseLL(Node *head){
-    Node *curr=head;
-    Node *prev=NULL;
-    Node *next=NULL;
-    while(curr!=NULL){
-        next=curr->next;
-        curr->next=curr->prev;
-        curr->prev=next;
-        prev=curr;
-        curr=next;
-
+Node *reverseLL(Node *head)
+{
+    Node *curr = head;
+    Node *prev = NULL;
+    Node *next = NULL;
+    while (curr != NULL)
+    {
+        next = curr->next;
+        curr->next = curr->prev;
+        curr->prev = next;
+        prev = curr;
+        curr = next;
     }
     return prev;
 }
-int main(){
-    Node * head=NULL;
-    Node * tail=NULL;
-    inserAtTail(head,tail,10);
-    inserAtTail(head,tail,20);
-    inserAtTail(head,tail,30);
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    inserAtTail(head, tail, 10);
+    inserAtTail(head, tail, 20);
+    inserAtTail(head, tail, 30);
     print(head);
-    Node *newhead=reverseLL(head);
+    Node *newhead = reverseLL(head);
     print(newhead);
-
-
 
     return 0;
 }
