3138d875b45fc9025881d1d4f0e30462572a967c
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-19 15:29:58 +0530

    FloyedCheck

--- LinkedList/floyedCycleDetection.cpp
+++ LinkedList/floyedCycleDetection.cpp
@@ -1,50 +1,58 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Node{
-    public:
+class Node
+{
+public:
     int data;
     Node *next;
-    Node(int data){
-        this->data=data;
-        this->next=NULL;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
     }
 };
-void print(Node *&head){
-    Node *temp=head;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
-        temp=temp->next;
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
     }
-    cout<<endl;
+    cout << endl;
 }
-bool floyedCheck(Node *&head){
-    Node *slow=head;
-    Node *fast=head;
+bool floyedCheck(Node *&head)
+{
+    Node *slow = head;
+    Node *fast = head;
 
-    while(fast!=NULL && fast->next!=NULL && slow!=NULL){
-            fast=fast->next->next;
-            slow=slow->next;
-            if(slow==fast){
-                return 1;
-            }
+    while (fast != NULL && fast->next != NULL && slow != NULL)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+        if (slow == fast)
+        {
+            return 1;
+        }
     }
     return 0;
 }
-int main(){
-    Node *head=new Node(3);
-    Node *node1=new Node(4);
-    Node *node2=new Node(5);
-    Node *node3=new Node(6);
-    Node *node4=new Node(7);
-    Node *node5=new Node(8);
-    head->next=node1;
-    node1->next=node2;
-    node2->next=node3;
-    node3->next=node4;
-    node4->next=node5;
-    node5->next=node3;
-    //print(head);
-    cout<<floyedCheck(head)<<endl;
+int main()
+{
+    Node *head = new Node(3);
+    Node *node1 = new Node(4);
+    Node *node2 = new Node(5);
+    Node *node3 = new Node(6);
+    Node *node4 = new Node(7);
+    Node *node5 = new Node(8);
+    head->next = node1;
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = node5;
+    node5->next = node3;
+    // print(head);
+    cout << floyedCheck(head) << endl;
 
     return 0;
 }
