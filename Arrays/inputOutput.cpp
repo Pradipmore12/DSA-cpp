95507223c799ab84a4dd5b03a791f5f3a56b7f39
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-20 23:07:39 +0530

    arrayPract

---
+++ Arrays/inputOutput.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdC++.h>
+using namespace std;
+int main(){ 
+    int n;
+    cin>>n;
+    int arr[100];
+    int t;
+    cin>>t;
+    while(t--){
+        for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    }
+    
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
