c8baa7e9191c9e3008b518b6f60491d8cc75f584
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-26 23:21:58 +0530

    mergeSortStart

--- Arrays/Sort/mergeSort.cpp
+++ Arrays/Sort/mergeSort.cpp
@@ -1,47 +1,79 @@
-#include<bits/stdC++.h>
+#include <bits/stdC++.h>
 using namespace std;
 
-//conquer Function
+// conquer Function
 
-void conquer(int arr[],int si,int mid,int ei){
-    int merged[(ei-si+1)];
-    int startIndexOfFirstArray=si;
-    int startIndexOfSecondArray=mid+1;
-    int startIndexOfMergedArray=0;
+void conquer(int arr[], int si, int mid, int ei)
+{
+    int merged[(ei - si + 1)];
+    int startIndexOfFirstArray = si;
+    int startIndexOfSecondArray = mid + 1;
+    int startIndexOfMergedArray = 0;
 
-    while(startIndexOfFirstArray<=mid && startIndexOfSecondArray<=ei){
-        if(arr[startIndexOfFirstArray]<=arr[startIndexOfSecondArray]){
-            merged[startIndexOfMergedArray]=arr[startIndexOfFirstArray];
+    while (startIndexOfFirstArray <= mid && startIndexOfSecondArray <= ei)
+    {
+        if (arr[startIndexOfFirstArray] <= arr[startIndexOfSecondArray])
+        {
+            merged[startIndexOfMergedArray] = arr[startIndexOfFirstArray];
             startIndexOfFirstArray++;
             startIndexOfMergedArray++;
         }
-        else {
-            merged[startIndexOfMergedArray]=arr[startIndexOfSecondArray];
+        else
+        {
+            merged[startIndexOfMergedArray] = arr[startIndexOfSecondArray];
             startIndexOfSecondArray++;
             startIndexOfMergedArray++;
         }
     }
 
+    while (startIndexOfFirstArray <= mid)
+    {
+        merged[startIndexOfMergedArray] = arr[startIndexOfFirstArray];
+        startIndexOfFirstArray++;
+        startIndexOfMergedArray++;
+    }
+    while (startIndexOfSecondArray <= ei)
+    {
+        merged[startIndexOfMergedArray] = arr[startIndexOfSecondArray];
+        startIndexOfSecondArray++;
+        startIndexOfMergedArray++;
+    }
+
+    // copying merged Array to Original array
+
+    for (int i = si, j = 0; i <= ei; i++, j++)
+    {
+        arr[i] = merged[j];
+    }
 }
 
-//divide Function
-void meregSortdivide(int arr[], int si, int ei){
-    if(si>=ei){
+// divide Function
+void meregSortdivide(int arr[], int si, int ei)
+{
+    if (si >= ei)
+    {
         return;
     }
-    int mid=si+(ei-si)/2;
-    meregSortdivide(arr,si,mid);
-    meregSortdivide(arr,mid+1,ei);
-    conquer(arr,si,mid,ei);
+    int mid = (si + ei) / 2;
+    meregSortdivide(arr, si, mid);
+    meregSortdivide(arr, mid + 1, ei);
+    conquer(arr, si, mid, ei);
 }
-int main(){
-    int arr[]={5,1,4,3,2};
-    int size=sizeof(arr)/sizeof(arr[0]);
-    cout<<size<<endl;
-    for(int i=0;i<size;i++){
-        cout<<arr[i]<<" ";
+int main()
+{
+    int arr[] = {5, 1, 4, 3, 2};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    cout << size << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
     }
-    meregSortdivide(arr,0,size-1);
+    cout << endl;
+    meregSortdivide(arr, 0, size - 1);
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }
