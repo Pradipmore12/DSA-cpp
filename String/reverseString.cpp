6f4f9e11d74452c6b76089ab0f6b51d92c521e14
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-27 22:36:59 +0530

    reverse

--- String/reverseString.cpp
+++ String/reverseString.cpp
@@ -1,15 +1,16 @@
-#include<bits/stdC++.h>
-#include<algorithm>
+#include <bits/stdC++.h>
+#include <algorithm>
 using namespace std;
-int main(){
+int main()
+{
     int num;
-    cin>>num;
+    cin >> num;
     string given;
-    cin>>given;
-    sort(given.begin(),given.end());
-    for(int i=given.length()-1;i>=0;i--){
-        cout<<given[i]<<"";
-
+    cin >> given;
+    sort(given.begin(), given.end());
+    for (int i = given.length() - 1; i >= 0; i--)
+    {
+        cout << given[i] << "";
     }
     return 0;
 }
