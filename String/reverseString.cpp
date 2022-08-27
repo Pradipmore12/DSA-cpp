793891a7b236e7c374f4ce3c580f6584ed8bcdb7
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-27 12:27:09 +0530

    mergeSortStart

---
+++ String/reverseString.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdC++.h>
+#include<algorithm>
+using namespace std;
+int main(){
+    int num;
+    cin>>num;
+    string given;
+    cin>>given;
+    sort(given.begin(),given.end());
+    for(int i=given.length()-1;i>=0;i--){
+        cout<<given[i]<<"";
+
+    }
+    return 0;
+}
