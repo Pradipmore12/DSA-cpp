8fc6c46276aa5848002ea9d19dfbeb38b459175f
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-28 21:51:03 +0530

    recursionprintfun

--- Recursion/sumOfN.cpp
+++ Recursion/sumOfN.cpp
@@ -4,13 +4,12 @@ int sum(int n)
 {
     if (n == 0)
         return 0;
-    return n + sum(n-1);
-
+    return n + sum(n - 1);
 }
 int main()
 {
     int n;
     cin >> n;
-    cout<<sum(n);
+    cout << sum(n);
     return 0;
 }
