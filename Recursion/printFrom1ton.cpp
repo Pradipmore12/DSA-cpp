7c5f580f3cacd153f5e18602d10234ae1586db2f
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-28 20:36:02 +0530

    recursionprintfun

--- Recursion/printFrom1ton.cpp
+++ Recursion/printFrom1ton.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-int cnt = 1;
+int cnt = 5;
 void print()
 {
-    if (cnt == 11)
+    if (cnt == 0)
         return;
     cout << cnt << " ";
-    cnt++;
+    cnt--;
     print();
 }
 int main()
