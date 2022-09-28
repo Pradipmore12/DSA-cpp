2dd4bec7f560703ec5b23b5df9fe869fd09154ef
Pradipmore12 <en19320803@git-india.edu.in>
2022-09-28 20:14:30 +0530

    stack start

--- Satck/implementation.cpp
+++ Satck/implementation.cpp
@@ -1,66 +1,71 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Stack{
-    public:
-        int size;
-        int *arr;
-        int top;
-    Stack(int N){
-        size=N;
-        top=-1;
-        arr=new int[size];
+class Stack
+{
+public:
+    int size;
+    int *arr;
+    int top;
+    Stack(int N)
+    {
+        size = N;
+        top = -1;
+        arr = new int[size];
     }
-    void push(int data){
-        if(top>=size-1){
-           cout<<"stack overflow"<<endl; 
+    void push(int data)
+    {
+        if (top >= size - 1)
+        {
+            cout << "stack overflow" << endl;
         }
         else
             top++;
-            arr[top]=data;
-        
+        arr[top] = data;
     }
-    void pop(){
-        if(top==-1){
-            cout<<"stack underflow"<<endl;
+    void pop()
+    {
+        if (top == -1)
+        {
+            cout << "stack underflow" << endl;
         }
-        else{
-            cout<<"popped element is :"<<arr[top]<<endl;
+        else
+        {
+            cout << "popped element is :" << arr[top] << endl;
             top--;
         }
-        
     }
-    int  peek(){
-        if(top==-1){
-            cout<<"there is no elements in stack"<<endl;
+    int peek()
+    {
+        if (top == -1)
+        {
+            cout << "there is no elements in stack" << endl;
         }
-        else{
+        else
+        {
             return arr[top];
         }
     }
-    bool empty(){
-        if(top==-1){
+    bool empty()
+    {
+        if (top == -1)
+        {
             return 1;
         }
         else
-            return 0; 
+            return 0;
     }
-
 };
-int main(){
+int main()
+{
     Stack s1(4);
     s1.push(3);
     s1.push(4);
     s1.push(5);
     s1.push(6);
-    cout<<s1.peek()<<endl;
+    cout << s1.peek() << endl;
     s1.pop();
-     cout<<s1.peek()<<endl;
-    s1.empty() ? cout<<"stack is empty"<<endl :cout<<"stack is nt empty"<<endl;
-    
-
-
-
-
+    cout << s1.peek() << endl;
+    s1.empty() ? cout << "stack is empty" << endl : cout << "stack is nt empty" << endl;
 
     return 0;
 }
